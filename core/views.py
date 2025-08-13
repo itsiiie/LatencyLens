49300d913b1f9216658e938916bc9ce8755de119
shashank <mdpkdy@gmail.com>
2025-08-14 05:23:21 +0530

    Database added

--- core/views.py
+++ core/views.py
@@ -1,13 +1,15 @@
 from django.http import JsonResponse
 from .latency_checker import check_latency
 from .models import LatencyResult
-
+from django.shortcuts import render
 SERVERS = [
     "https://www.google.com",
     "https://www.github.com"
 ]
 def home(request):
     return ("welcome")
+
+
 def healthz(request):
     latencies = check_latency(SERVERS)
 
@@ -16,3 +18,9 @@ def healthz(request):
         LatencyResult.objects.create(server=server, latency_ms=latency)
 
     return JsonResponse({"status": "ok", "latencies": latencies})
+
+
+def dashboard(request):
+    # Get last 50 latency results, newest first
+    latency_results = LatencyResult.objects.order_by('-checked_at')[:50]
+    return render(request, 'core/dashboard.html', {'latency_results': latency_results})
